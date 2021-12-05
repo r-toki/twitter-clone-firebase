3fba21a9ad11f2fedce74f4666559b8486b0a5fd
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-12-05 14:27:32 +0900

    feat: Home にレイアウトを追加

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,26 +1,36 @@
 import type { NextPage } from "next";
+import { ReactNode, VFC } from "react";
 
 const Home: NextPage = () => {
+  return <HomeLayout main={<Main />} left={<Left />} right={<Right />} />;
+};
+
+export default Home;
+
+type HomeLayoutProps = {
+  main: ReactNode;
+  left: ReactNode;
+  right: ReactNode;
+};
+
+const HomeLayout: VFC<HomeLayoutProps> = ({ main, left, right }) => {
   return (
-    <div className="alert alert-error">
-      <div className="flex-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="w-6 h-6 mx-2 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-          ></path>
-        </svg>
-        <label>Lorem ipsum dolor sit amet, consectetur adip!</label>
-      </div>
+    <div className="flex min-h-screen">
+      <div className="w-64">{left}</div>
+      <div className="flex-1">{main}</div>
+      <div className="w-64">{right}</div>
     </div>
   );
 };
 
-export default Home;
+const Main: VFC = () => {
+  return <div>Main</div>;
+};
+
+const Left: VFC = () => {
+  return <div>Left</div>;
+};
+
+const Right: VFC = () => {
+  return <div>Right</div>;
+};
