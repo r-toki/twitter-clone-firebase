30cde2f10466cb303bf153b3c41344dea214486a
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-12-25 16:02:28 +0900

    a

--- client/pages/sign_up/index.tsx
+++ client/pages/sign_up/index.tsx
@@ -1,7 +1,22 @@
 import { Box } from "@chakra-ui/react";
+import { serverTimestamp } from "firebase/firestore";
 import { NextPage } from "next";
+import { useEffect } from "react";
+
+import { typedUsersRef, UserData, userPath } from "@/infra/firestore";
+
+const userData: UserData = {
+  displayName: "",
+  selfIntroduction: "",
+  avatar: null,
+  createdAt: serverTimestamp() as any,
+  updatedAt: serverTimestamp() as any,
+};
 
 const SignUpPage: NextPage = () => {
+  useEffect(() => {
+    console.log(typedUsersRef);
+  });
   return <Box height="100vh" bg="gray.100"></Box>;
 };
 
