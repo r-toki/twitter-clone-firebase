d8a1c9831087eec7237a7f322adabed8d1fc3f7e
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-12-26 22:08:05 +0900

    a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,27 @@
 import "./config/firebaseApp";
 
-import { functions } from "./infra/functions";
+import * as admin from "firebase-admin";
+import * as _functions from "firebase-functions";
 
-export const echo = functions.https.onCall((data, ctx) => {
-  return data;
+const functions = _functions.region("asia-northeast1");
+const bucket = admin.storage().bucket();
+
+const toFileData = (encoded: string) => encoded.replace(/^data:\w+\/\w+;base64,/, "");
+
+const toContentType = (encoded: string) =>
+  encoded.toString().slice(encoded.indexOf(":") + 1, encoded.indexOf(";"));
+
+export const uploadFile = functions.https.onCall(async (encoded: string, ctx) => {
+  const buffer = Buffer.from(toFileData(encoded), "base64");
+  const contentType = toContentType(encoded);
+
+  const file = bucket.file("users/user1");
+  await file.save(buffer, { metadata: { contentType } });
+
+  const metadata = await file.getMetadata();
+  console.log("metadata", metadata[1].body.mediaLink);
+
+  return {
+    encoded,
+  };
 });
--- functions/src/infra/functions/index.ts
+++
@@ -1,3 +0,0 @@
-import * as _functions from "firebase-functions";
-
-export const functions = _functions.region("asia-northeast1");
