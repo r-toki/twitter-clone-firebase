655782fec693e68197ca182dd53fb4d42fd29dcc
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-12-28 17:43:09 +0900

    a

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,41 +3,61 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 
 import { uploadFile } from "@/infra/functions";
+import { addDoc, getDocs, Timestamp } from "firebase/firestore";
+import { typedUsersRef } from "@/infra/firestore";
 
-const toBase64 = (file: File): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });
+// const toBase64 = (file: File): Promise<string> =>
+//   new Promise((resolve, reject) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => resolve(reader.result as string);
+//     reader.onerror = (error) => reject(error);
+//   });
 
-const Home: NextPage = () => {
-  const [avatar, setAvatar] = useState<File | undefined>();
+// const Home: NextPage = () => {
+//   const [avatar, setAvatar] = useState<File | undefined>();
 
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (!avatar) return;
-        toBase64(avatar).then(uploadFile);
-      }}
-    >
-      <input
-        type="file"
-        name="avatar"
-        onChange={(e) => {
-          const files = e.target.files;
-          if (!files) {
-            setAvatar(undefined);
-          } else {
-            setAvatar(files[0]);
-          }
-        }}
-      />
-      <button>Submit</button>
-    </form>
-  );
-};
+//   return (
+//     <form
+//       onSubmit={(e) => {
+//         e.preventDefault();
+//         if (!avatar) return;
+//         toBase64(avatar).then(uploadFile);
+//       }}
+//     >
+//       <input
+//         type="file"
+//         name="avatar"
+//         onChange={(e) => {
+//           const files = e.target.files;
+//           if (!files) {
+//             setAvatar(undefined);
+//           } else {
+//             setAvatar(files[0]);
+//           }
+//         }}
+//       />
+//       <button>Submit</button>
+//     </form>
+//   );
+// };
 
-export default Home;
+// export default Home;
+
+export default function () {
+  useEffect(() => {
+    addDoc(typedUsersRef(), {
+      displayName: "",
+      selfIntroduction: "",
+      avatar: null,
+      createdAt: Timestamp.now(),
+      updatedAt: Timestamp.now(),
+    }).then(() => {
+      getDocs(typedUsersRef()).then((snap) => {
+        const users = snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        console.log(users);
+      });
+    });
+  }, []);
+  return <Box>Home</Box>;
+}
