b4b98d62fac0131f5a13d8833e83e1f49c509b1a
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-01-09 17:48:03 +0900

    a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,22 +6,25 @@ import * as _functions from "firebase-functions";
 const functions = _functions.region("asia-northeast1");
 const bucket = admin.storage().bucket();
 
-const toFileData = (encoded: string) => encoded.replace(/^data:\w+\/\w+;base64,/, "");
+const toFileData = (encoded: string) =>
+  encoded.replace(/^data:\w+\/\w+;base64,/, "");
 
 const toContentType = (encoded: string) =>
   encoded.toString().slice(encoded.indexOf(":") + 1, encoded.indexOf(";"));
 
-export const uploadFile = functions.https.onCall(async (encoded: string, ctx) => {
-  const buffer = Buffer.from(toFileData(encoded), "base64");
-  const contentType = toContentType(encoded);
+export const uploadFile = functions.https.onCall(
+  async (encoded: string, ctx) => {
+    const buffer = Buffer.from(toFileData(encoded), "base64");
+    const contentType = toContentType(encoded);
 
-  const file = bucket.file("users/user1");
-  await file.save(buffer, { metadata: { contentType } });
+    const file = bucket.file("users/user1");
+    await file.save(buffer, { metadata: { contentType } });
 
-  const metadata = await file.getMetadata();
-  console.log("metadata", metadata[1].body.mediaLink);
+    const metadata = await file.getMetadata();
+    console.log("metadata", metadata[1].body.mediaLink);
 
-  return {
-    encoded,
-  };
-});
+    return {
+      encoded,
+    };
+  }
+);
