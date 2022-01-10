edb629231d6cc3f2c22cb1419015defe93d73b41
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-01-10 19:37:13 +0900

    a

--- client/lib/$path.ts
+++ client/lib/$path.ts
@@ -4,6 +4,11 @@ export const pagesPath = {
   sign_up: {
     $url: (url?: { hash?: string }) => ({ pathname: '/sign_up' as const, hash: url?.hash })
   },
+  users: {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/users/[id]' as const, query: { id }, hash: url?.hash })
+    })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
